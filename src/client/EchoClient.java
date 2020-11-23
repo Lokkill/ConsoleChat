d7185806596a785293c33ad05f31c0c0d8cb716d
Andrey Kataev <corvolk@yandex.ru>
2020-11-23 21:38:57 +0300

    Initial commit

---
+++ src/client/EchoClient.java
@@ -0,0 +1,9 @@
+package client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+}
---
+++ src/server/EchoServer.java
@@ -0,0 +1,12 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoServer {
+
+}
