dec08a758df007191c4647adf266a6bd04375ebe
Andrey Kataev <corvolk@yandex.ru>
2020-11-23 21:39:58 +0300

    Completed Home work

--- src/client/EchoClient.java
+++ src/client/EchoClient.java
@@ -6,4 +6,86 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class EchoClient {
+    private final String SERVER_ADDRESS = "localhost";
+    private final int SERVER_PORT = 8189;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public EchoClient() {
+        try {
+            openConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openConnection() throws Exception {
+        socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        readMessage();
+        sendMessage();
+    }
+
+    private void readMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        String fromServer = in.readUTF();
+                        if (fromServer.equals("/end")) {
+                            closeConnection();
+                            break;
+                        }
+                        System.out.println("Server: " + fromServer);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void closeConnection() {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Scanner scanner = new Scanner(System.in);
+                while (true) {
+                    try {
+                        String outString = scanner.nextLine();
+                        //System.out.println("User: " + outString);
+                        out.writeUTF(outString);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    new EchoClient();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 }
--- src/server/EchoServer.java
+++ src/server/EchoServer.java
@@ -8,5 +8,55 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class EchoServer {
+    private static DataInputStream in;
+    private static DataOutputStream out;
+    private static Socket socket = null;
+
+    public static void main(String[] args) {
+        readMessage();
+        writeMessage();
+    }
+
+    private static void writeMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Scanner scanner = new Scanner(System.in);
+                while (true) {
+                    String strFromServer = scanner.nextLine();
+                    try {
+                        //System.out.println("Server: " + strFromServer);
+                        out.writeUTF(strFromServer);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private static void readMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try (ServerSocket serverSocket = new ServerSocket(8189)) {
+                    System.out.println("Server starting...");
+                    socket = serverSocket.accept();
+                    System.out.println("Client is connect");
+                    in = new DataInputStream(socket.getInputStream());
+                    out = new DataOutputStream(socket.getOutputStream());
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.equals("/exit")) {
+                            break;
+                        }
+                        System.out.println("User: " + str);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 
 }
